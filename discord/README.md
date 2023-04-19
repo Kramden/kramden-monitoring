bd58e760a082bff8a16ec0b3eee50163ec3507a5
Ken VanDine <ken@vandine.org>
2023-04-19 16:40:02 -0400

    Added skeleton discord bot

---
+++ discord/README.md
@@ -0,0 +1,13 @@
+# Kramden discord bot
+
+Install necessary dependencies, discord and lars python libraries are required
+
+`
+pip3 install discord lars
+`
+
+To run:
+
+`
+KRAMDEN_DISCORD_TOKEN=<TOKEN> python3 kramden.py
+`
---
+++ discord/kramden.py
@@ -0,0 +1,35 @@
+import os
+import discord
+
+client = discord.Client()
+
+usage = '''\
+Welcome to the Kramden bot!
+
+Available commands:
+    -stats 
+    -up
+'''.format(length='multi-line', ordinal='second')
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('-stats'):
+        await message.channel.send('FIXME')
+
+    if message.content.startswith('-up'):
+        await message.channel.send('FIXME')
+    if message.content.startswith('-help'):
+        await message.channel.send(usage)
+
+token = os.getenv('KRAMDEN_DISCORD_TOKEN')
+if token:
+  client.run(token)
+else:
+  print("KRAMDEN_DISCORD_TOKEN environment variable required")
